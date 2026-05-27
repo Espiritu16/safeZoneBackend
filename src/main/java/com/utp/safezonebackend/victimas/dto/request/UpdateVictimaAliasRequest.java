27473ab56e8988d883d288c01d0b5e85ed211a7b
salvagd09 <vasco14_100@hotmail.com>
2026-05-26 19:35:00 -0500

    Backend del RF11 hecho

--- src/main/java/com/utp/safezonebackend/victimas/dto/request/UpdateVictimaAliasRequest.java
+++ src/main/java/com/utp/safezonebackend/victimas/dto/request/UpdateVictimaAliasRequest.java
@@ -1,4 +1,16 @@
 package com.utp.safezonebackend.victimas.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.OffsetDateTime;
+
 public class UpdateVictimaAliasRequest {
+    @NotBlank
+    private String actualizadoPor;
+    public String getActualizadoPor() {
+        return actualizadoPor;
+    }
+    public void setActualizadoPor(String actualizadoPor) {
+        this.actualizadoPor = actualizadoPor;
+    }
 }
--- src/main/java/com/utp/safezonebackend/victimas/entity/VictimaAlias.java
+++ src/main/java/com/utp/safezonebackend/victimas/entity/VictimaAlias.java
@@ -1,9 +1,7 @@
 package com.utp.safezonebackend.victimas.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 import java.time.OffsetDateTime;
 
 @Entity
@@ -14,6 +12,8 @@ public class VictimaAlias {
     private String id;
 
     @Column(name = "victima_id")
+    @OneToOne
+    @JoinColumn(name="id_usuario")
     private String victimaId;
 
     @Column(name = "alias_codigo")
@@ -41,4 +41,70 @@ public class VictimaAlias {
 
     @Column(name = "fecha_inactivacion")
     private OffsetDateTime fechaInactivacion;
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getVictimaId() {
+        return victimaId;
+    }
+    public void setVictimaId(String victimaId) {
+        this.victimaId = victimaId;
+    }
+    public String getAliasCodigo() {
+        return aliasCodigo;
+    }
+    public void setAliasCodigo(String aliasCodigo) {
+        this.aliasCodigo = aliasCodigo;
+    }
+    public boolean isActivo() {
+        return activo;
+    }
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+    public OffsetDateTime getFechaAsignacion() {
+        return fechaAsignacion;
+    }
+    public void setFechaAsignacion(OffsetDateTime fechaAsignacion) {
+        this.fechaAsignacion = fechaAsignacion;
+    }
+    public OffsetDateTime getFechaFin() {
+        return fechaFin;
+    }
+    public void setFechaFin(OffsetDateTime fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+    public String getCreadoPor() {
+        return creadoPor;
+    }
+    public void setCreadoPor(String creadoPor) {
+        this.creadoPor = creadoPor;
+    }
+    public String getActualizadoPor() {
+        return actualizadoPor;
+    }
+    public void setActualizadoPor(String actualizadoPor) {
+        this.actualizadoPor = actualizadoPor;
+    }
+    public String getInactivadoPor() {
+        return inactivadoPor;
+    }
+    public void setInactivadoPor(String inactivadoPor) {
+        this.inactivadoPor = inactivadoPor;
+    }
+    public OffsetDateTime getFechaActualizacion() {
+        return fechaActualizacion;
+    }
+    public void setFechaActualizacion(OffsetDateTime fechaActualizacion) {
+        this.fechaActualizacion = fechaActualizacion;
+    }
+    public OffsetDateTime getFechaInactivacion() {
+        return fechaInactivacion;
+    }
+    public void setFechaInactivacion(OffsetDateTime fechaInactivacion) {
+        this.fechaInactivacion = fechaInactivacion;
+    }
 }
